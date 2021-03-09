8662c5e0fdf6a1a082314b308ea6a6c0cc1390c5
ravinduf <ravinduf@rocketmail.com>
2021-03-09 10:48:54 +0530

    cleaned code base

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 
 import './assets/stylesheets/main.scss'
 
 function App() {
   return (
     <div>
-      Hello
+
     </div>
   );
 }
