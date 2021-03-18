7f0dc5d0ed50bc3da608f0b526da5181f8582c28
ravinduf <ravinduf@rocketmail.com>
2021-03-18 07:50:19 +0530

    created welcome-ux

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -1,1 +1,28 @@
-export {}
+import React from 'react';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import welcomeImg from '../../assets/images/welcome.jpg';
+
+const Welcome = () => {
+  return (
+      <Container fluid>
+        <Row className="heading">
+          My Library
+        </Row>
+        <Row>
+          <img src={welcomeImg} width="100%" alt="Library"/>
+        </Row>
+
+        <Row className="credits">
+          <p className="pr-2">Photo by <a
+              href="https://unsplash.com/@annahunko?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Anna
+            Hunko</a> on <a
+              href="https://unsplash.com/@annahunko?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a>
+          </p>
+        </Row>
+      </Container>
+  )
+}
+
+export default Welcome;
