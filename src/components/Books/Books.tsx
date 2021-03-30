4fcaa93351027b22df0615f5baf55950b2bcc051
ravinduf <ravinduf@rocketmail.com>
2021-03-30 17:49:15 +0530

    books-crud done

--- src/components/Books/Books.tsx
+++ src/components/Books/Books.tsx
@@ -1,19 +1,59 @@
 import React, {useState} from "react";
-
 import BooksTitle from "./BooksTitle/BooksTitle";
 import BooksList from "./BooksList/BooksList";
 import AddBook from "./AddBook/AddBook";
+import {IBook} from "../../types/Book";
+import {IAuthor} from "../../types/Author";
 
+type BooksProps = {
+  authors: IAuthor[],
+}
 
+const Books: React.FC<BooksProps> = ({authors}) => {
 
-const Books = () => {
+  const [books, setBooks] = useState<IBook[]>([])
+  const [updateBook, setUpdateBook] = useState<IBook | null>(null)
+
+  const handleSetUpdateBook = (book: IBook | null) => {
+    setUpdateBook(book)
+  }
+
+  const addBook = (newBook: IBook) => {
+    newBook.id = books.length
+    const newBooksList: IBook[] = books.slice();
+    newBooksList.push(newBook)
+    setBooks(newBooksList)
+  }
+
+  const deleteBook = (index: number) => {
+    const newBooks = books.slice();
+    newBooks.splice(index, 1);
+    setBooks(newBooks)
+  }
+
+  const handleUpdateBookList = () => {
+    if (updateBook) {
+      const index = updateBook.id
+      books.splice(index, 1, updateBook)
+    }
+  }
 
   return (
-    <React.Fragment>
-      <BooksTitle/>
-      <BooksList/>
-      <AddBook/>
-    </React.Fragment>
+      <React.Fragment>
+        <BooksTitle/>
+        <BooksList
+            books={books}
+            deleteBook={deleteBook}
+            handleSetUpdateBook={handleSetUpdateBook}
+        />
+        <AddBook
+            addBook={addBook}
+            authors={authors}
+            updateBook={updateBook}
+            handleSetUpdateBook={handleSetUpdateBook}
+            handleUpdateBookList={handleUpdateBookList}
+        />
+      </React.Fragment>
   );
 };
 
--- src/components/Books/BooksList/BooksListItem/BooksListItem.tsx
+++ src/components/Books/BooksList/BooksListItem/BooksListItem.tsx
@@ -2,20 +2,35 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import {Edit, Trash2} from "react-feather";
+import {IBook} from "../../../../types/Book";
 
+type bookProps = {
+  book: IBook,
+  index: number,
+  deleteBook: (index: number) => void,
+  handleSetUpdateBook: (book: IBook | null) => void,
+}
 
-const BooksListItem = () => {
+const BooksListItem: React.FC<bookProps> = ({book, index, deleteBook, handleSetUpdateBook}) => {
   return (
       <Container>
         <Row className="d-flex justify-content-between book-item">
-          <h4>book</h4>
+          <h4>{index + 1}. {book.title}</h4>
           <span className="action-btns">
-            <Edit className="text-warning mr-2" />
-            <Trash2 
-              className="text-danger mr-2" 
-              
-            />
-        </span>
+                    <Edit 
+                      className="text-warning mr-2" 
+                      onClick={() => {
+                          handleSetUpdateBook(book)
+                        }}
+                    />
+                    <Trash2 
+                      className="text-danger mr-2" 
+                      onClick={() => {
+                        if (window.confirm("press 'ok' to delete book"))
+                          deleteBook(index)
+                      }}
+                    />
+                </span>
         </Row>
       </Container>
   );
